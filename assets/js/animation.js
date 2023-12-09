cf1a66343c10f3836530d6bba5fd6293b9be0e35
lasthour <hourabdo89@gmail.com>
2023-12-09 17:59:18 +0100

    some animttion

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -6,6 +6,8 @@ gsap.to(".delimiter", {
     yoyo: true,
   });
 
+
+// timeLine for writeter words
 gsap.defaults({ease: "none"});
 const tl = gsap.timeline({repeat:-1, repeatDelay:3, yoyo:true});
 tl.to("#name", {
@@ -24,3 +26,68 @@ tl.to("#ImSoft", {
   color: "hsl(230, 80%, 90%)",
   textShadow: "0 0 1em hsla(320, 100%, 50%, 0.5), 0 0 0.125em hsla(320, 100%, 60%, 0.5), -0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2), 0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4)"
 });
+
+
+
+//time line for scrolling
+
+
+const t_scroll = gsap.timeline()
+// t_scroll.from('.paraIamsoft', {
+  
+//   scrollTrigger: {
+//     trigger: '.paraIamsoft',
+//     markers: true,
+//     start: "top 70%",
+//     end: "top 10%",
+//     scrub: 8,
+    
+//   },
+//   ease: "power2.out",
+//   x: -1500,
+//   // y: 50,
+//   opacity: 0,
+//   })
+
+
+//work exp scroll
+
+t_scroll.from('#titl_workexp',{
+              scrollTrigger: {
+                trigger: '#titl_workexp',
+                // markers: true,
+                start: "top 70%",
+                end: "-20% 10%",
+                scrub: 8,
+                
+              },
+              ease: "power2.out",
+              x: -1500,
+              opacity: 0,
+            })
+t_scroll.from('.main_container',{
+  scrollTrigger: {
+    trigger: '.main_container',
+    // markers: true,
+    start: "top 100%",
+    end: "-20% 10%",
+    scrub: 8,
+    
+  },
+  ease: "power2.out",
+  x: -1500,
+  opacity: 0,
+})
+
+
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  console.log('scroll')
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
